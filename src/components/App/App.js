4a445f5f5600909b4661c4b758182f5c42fe44c7
Karolina Kovdya <karolina.kovdya-onikienko@yandex.ru>
2023-04-24 20:46:55 +0300

    добавить компонент AboutProject

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from '../Haeder/Header';
-import Promo from '../Promo/Promo';
+import Main from '../Main/Main';
 
 function App() {
   return (
     <div className="app">
-      <Header></Header>
-      <Promo></Promo>
+      <Header />
+      <Main />
     </div>
   );
 }
---
+++ src/components/Main/Main.js
@@ -0,0 +1,13 @@
+import Promo from '../Promo/Promo';
+import AboutProject from '../AboutProject/AboutProject';
+
+function Main () {
+    return (
+      <div className="main">
+        <Promo></Promo>
+        <AboutProject></AboutProject>
+      </div>
+    )
+}
+
+export default Main;
