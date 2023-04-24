3d8dc2f2af99178a3bd1c00f1d97f1c2a4dcf461
Karolina Kovdya <karolina.kovdya-onikienko@yandex.ru>
2023-04-24 13:34:30 +0300

    добавить компонент Promo

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from '../Haeder/Header';
+import Promo from '../Promo/Promo';
 
 function App() {
   return (
     <div className="app">
       <Header></Header>
+      <Promo></Promo>
     </div>
   );
 }
---
+++ src/components/Promo/Promo.js
@@ -0,0 +1,17 @@
+import './Promo.css'
+import text from '../../images/promoImg.svg'
+
+function Promo () {
+    return (
+        <div className="promo">
+            <div className='promo__text'>
+              <h1 className="promo__title">Учебный проект студента факультета Веб-разработки.</h1>
+              <p className="promo__info">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
+              <button className='promo_btn'>Узнать больше</button>  
+            </div>
+            <img className="promo__img" src={text} alt="Изображение буквы" />
+        </div>
+    )
+}
+
+export default Promo;
