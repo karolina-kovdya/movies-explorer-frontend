eea1612a29476b4314649ad8b2c95958535a6967
Karolina Kovdya <karolina.kovdya-onikienko@yandex.ru>
2023-04-28 19:57:31 +0300

    добавляет компоненты EntryForm && Register

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Header from '../Haeder/Header';
 import Main from '../Main/Main';
 import Footer from '../Footer/Footer';
+import Register from '../Register/Register';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Header />
         <Main />
         <Footer />
+        <Register />
       </div>
     </BrowserRouter>
   );
---
+++ src/components/Register/Register.js
@@ -0,0 +1,47 @@
+import "./Register.css";
+import EntryForm from "../EntryForm/EntryForm";
+
+function Register() {
+  return (
+    <EntryForm
+      name="register"
+      title="Добро пожаловать!"
+      buttonText="Зарегистрироваться"
+      text="Уже зарегистрированы?"
+      url='/signin'
+      linkText="Войти"
+    >
+      <>
+        <label className="register__field">
+          <h3 className="register-placeholder">Имя</h3>
+          <input
+          className="register__input"
+            type="text"
+            name="name"
+            required
+          />
+        </label>
+        <label className="register__field">
+          <h3 className="register-placeholder">E-mail</h3>
+          <input
+          className="register__input"
+            type="text"
+            name="email"
+            required
+          />
+        </label>
+        <label className="register__field">
+          <h3 className="register-placeholder">Пароль</h3>
+          <input
+          className="register__input"
+            type="text"
+            name="password"
+            required
+          />
+        </label>
+      </>
+    </EntryForm>
+  );
+}
+
+export default Register;
